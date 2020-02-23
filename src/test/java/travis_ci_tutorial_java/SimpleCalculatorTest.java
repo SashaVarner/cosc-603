611b9ce278704e02899e0bafc475f20e6476da3f
sashavarner <sasha.pivowar@gmail.com>
2020-02-23 16:11:30 -0500

    Created test for multiplication

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,6 +1,7 @@
 package travis_ci_tutorial_java;
 
 import org.junit.Test;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,6 +17,12 @@ public class SimpleCalculatorTest {
 	public void testSubtract() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.sub(5,3),2);
+	}
+
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.mult(1,2), 2);
 	}
 }
 
